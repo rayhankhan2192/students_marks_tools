68901c3061e35213c2f6828f823321c453feedbf
Rayhan Khan <rayhankhan1226@gmail.com>
2025-01-06 01:35:25 +0600

    Added user model and OTP model

--- baseapp/accountsapp/models.py
+++ baseapp/accountsapp/models.py
@@ -1,3 +1,74 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+from django.utils.timezone import now
+from datetime import timedelta
 
-# Create your models here.
+class AccountManager(BaseUserManager):
+    def create_user(self, email, first_name, last_name, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+
+        email = self.normalize_email(email)
+        user = self.model(
+            email=email,
+            first_name=first_name,
+            last_name=last_name,
+            **extra_fields
+        )
+        user.set_password(password)  # Hash the password
+        user.save(using=self._db)
+        return user
+
+    
+    def create_superuser(self, first_name, last_name, email, password=None, **extra_fields):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name,
+            password=password,  
+            **extra_fields
+        )
+        user.is_admin = True
+        user.is_active = True
+        user.is_staff = True 
+        user.is_superadmin = True
+        user.save(using=self._db)  # Corrected argument
+        return user
+
+
+class Account(AbstractBaseUser):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=60, unique=True)
+    
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now_add=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    is_superadmin = models.BooleanField(default=False)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+    
+    objects = AccountManager()
+    
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+    
+    def __str__(self):
+        return self.email
+    
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+    
+    def has_module_perms(self, add_label):
+        return True
+    
+class OTP(models.Model):
+    user = models.ForeignKey('Account', on_delete=models.CASCADE, related_name='otps')
+    otp_code = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def is_expired(self):
+        return now() > self.created_at + timedelta(seconds=90)
